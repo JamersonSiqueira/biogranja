d95198fab7ebc805eb8de45c3f30aee9c4aa97d4
Jamerson Siqueira <jamerson.ss16@gmail.com>
2020-05-06 21:46:35 -0300

    update 06-05-2020-2
    pasta adicionado frontend

--- projeto netbeans/BioGranja/src/br/com/biogranja/view/mainView.java
+++ projeto netbeans/BioGranja/src/br/com/biogranja/view/mainView.java
@@ -27,23 +27,23 @@ public class mainView extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
+        btnInterno = new javax.swing.JButton();
+        btnClient = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jButton1.setText("Acessar como Funcionário");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        btnInterno.setText("Acessar como Funcionário");
+        btnInterno.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                btnInternoActionPerformed(evt);
             }
         });
 
-        jButton2.setText("Acessar como Cliente");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        btnClient.setText("Acessar como Cliente");
+        btnClient.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                btnClientActionPerformed(evt);
             }
         });
 
@@ -56,37 +56,40 @@ public class mainView extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jButton2)
+                .addComponent(btnClient)
                 .addGap(131, 131, 131))
             .addGroup(layout.createSequentialGroup()
-                .addGap(121, 121, 121)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel1)
-                    .addComponent(jButton1))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(121, 121, 121)
+                        .addComponent(btnInterno))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(150, 150, 150)
+                        .addComponent(jLabel1)))
                 .addContainerGap(120, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(51, Short.MAX_VALUE)
+                .addContainerGap(20, Short.MAX_VALUE)
                 .addComponent(jLabel1)
-                .addGap(33, 33, 33)
-                .addComponent(jButton2)
+                .addGap(18, 18, 18)
+                .addComponent(btnClient)
                 .addGap(27, 27, 27)
-                .addComponent(jButton1)
-                .addGap(127, 127, 127))
+                .addComponent(btnInterno)
+                .addGap(37, 37, 37))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
+    private void btnInternoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnInternoActionPerformed
+        new viewInterno().setVisible(true);
+    }//GEN-LAST:event_btnInternoActionPerformed
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton2ActionPerformed
+    private void btnClientActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnClientActionPerformed
+        new viewCliente().setVisible(true);
+    }//GEN-LAST:event_btnClientActionPerformed
 
     /**
      * @param args the command line arguments
@@ -124,8 +127,8 @@ public class mainView extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
+    private javax.swing.JButton btnClient;
+    private javax.swing.JButton btnInterno;
     private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
